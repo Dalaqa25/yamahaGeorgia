509dc6b594f737048995ef2d4e9714cf97e93dab
dalaqa25 <g.dalaqishvili01@gmail.com>
2025-02-27 23:40:19 +0400

    testing

---
+++ mainfront/src/components/homeComponents/mainHome.jsx
@@ -0,0 +1,8 @@
+export default function MainHome() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>Welcome to our site!</p>
+        </div>
+    )
+}
--- mainfront/src/routes/home.jsx
+++ mainfront/src/routes/home.jsx
@@ -1,8 +1,9 @@
+import MainHome from "../components/homeComponents/mainHome"
+
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-            <p>Welcome to our site!</p>
-        </div>
+        <>
+            <MainHome/>
+        </>
     )
 }
