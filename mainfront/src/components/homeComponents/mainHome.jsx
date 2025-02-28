b5788e672ee25b35f4a2e01c5d88defe02b4261a
dalaqa25 <g.dalaqishvili01@gmail.com>
2025-02-28 19:33:25 +0400

    state changes

--- mainfront/src/components/homeComponents/mainHome.jsx
+++ mainfront/src/components/homeComponents/mainHome.jsx
@@ -1,11 +1,30 @@
 import '../../assets/home.css';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react'
+
 export default function MainHome() {
+
+    const h2Text = ["faster.", "better.", "safely.", "more reliable."]
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const [fade, setFade] = useState(true)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setFade(false)
+            setTimeout(() => {
+                setCurrentIndex((prevIndex) => (prevIndex + 1) % h2Text.length)
+                setFade(true)
+            }, 500)
+        }, 5000)
+
+        return () => clearInterval(interval)
+    }, [h2Text.length])
+
     return (
         <>
             <div className='mainTxt'>
                 <h1>Ride your bike</h1>
-                <h2>faster.</h2>
+                <h2 className={fade ? 'fade-in' : 'fade-out'}>{h2Text[currentIndex]}</h2>
                 <p>Yamaha Motors is a Japanese company known for motorcycles,<br/> marine products, and power equipment.</p>
             </div>
             <div className='btnContainer'>
