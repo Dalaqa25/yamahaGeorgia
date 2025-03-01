e06990f2d86fd17e10c8c92b9394ebed559773fa
dalaqa25 <g.dalaqishvili01@gmail.com>
2025-03-02 00:33:52 +0400

    ds

--- mainfront/src/App.jsx
+++ mainfront/src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/navbar"
 import { Routes, Route, useLocation } from "react-router"
+import { useEffect } from "react"
 import Home from "./routes/home"
 import Contact from "./routes/contact"
 import Motorcycles from "./routes/motorcycles"
@@ -10,6 +11,21 @@ function App() {
 
   const location = useLocation();
   const isHome = location.pathname === "/";
+  const isMotorcycles = location.pathname === "/motorcycles";
+  const isAccessories = location.pathname === "/accessories";
+
+  useEffect(() => {
+    const navrbarSwich = () => {
+      const navbar = document.querySelector('ul')
+      if (location.pathname === isMotorcycles || isAccessories) {
+        navbar.classList.add('switched')
+      } else {
+        navbar.classList.remove('switched')
+      }
+    };
+
+    navrbarSwich();
+  }, [isMotorcycles,isAccessories])
 
   return (
     <>
