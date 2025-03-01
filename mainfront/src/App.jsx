fedb3bdfd71c8f04f8ab4d182cd92c1ec0edecf8
dalaqa25 <g.dalaqishvili01@gmail.com>
2025-03-01 22:58:26 +0400

    ds

--- mainfront/src/App.jsx
+++ mainfront/src/App.jsx
@@ -1,21 +1,27 @@
 import Navbar from "./components/navbar"
-
-import { Routes, Route } from "react-router"
+import { Routes, Route, useLocation } from "react-router"
 import Home from "./routes/home"
 import Contact from "./routes/contact"
 import Motorcycles from "./routes/motorcycles"
 import Accessories from "./routes/accessories"
+import './index.css';
 
 function App() {
+
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
   return (
     <>
       <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="contact" element={<Contact />}/>
-        <Route path="motorcycles" element={<Motorcycles />}/>
-        <Route path="accessories" element={<Accessories />}/>
-      </Routes>
+      <div className={isHome ? "" : "bodyMain"}>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="contact" element={<Contact />}/>
+          <Route path="motorcycles" element={<Motorcycles />}/>
+          <Route path="accessories" element={<Accessories />}/>
+        </Routes>
+      </div>
     </>
   )
 }
