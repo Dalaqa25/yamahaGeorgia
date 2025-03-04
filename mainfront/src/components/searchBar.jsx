0c12974c2676ded07076df45a4234dbcb3a7446f
dalaqa25 <g.dalaqishvili01@gmail.com>
2025-03-04 11:05:22 +0400

    ds

--- mainfront/src/components/searchBar.jsx
+++ mainfront/src/components/searchBar.jsx
@@ -1,13 +1,44 @@
 import '../../src/assets/searchbar.css';
 import searchSVG from '../../src/assets/images/searchSVG.svg';
+import { useState } from 'react';
 
 export default function SearchBar() {
+    const [input, setInput] = useState('');
+
+    const fetchData = (value) => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((response) => response.json())
+            .then(json => {
+                const filteredData = json.filter((user) => {
+                    return value &&
+                     user &&
+                      user.name && 
+                      user.name.toLowerCase().includes(value.toLowerCase());
+                });
+                console.log(filteredData);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    };
+
+    const handleChange = (value) => {
+        setInput(value);
+        fetchData(value);
+    };
+
     return (
         <div className="search-bar">
             <div className='search-bar-wrapper'>
-                <input type="text" style={{textAlign:'center'}} placeholder="Search for a product" />
-                <img src={searchSVG} alt="" />
+                <input
+                    type="text"
+                    style={{ textAlign: 'center' }}
+                    placeholder="Search for a product"
+                    value={input}
+                    onChange={(e) => handleChange(e.target.value)}
+                />
+                <img src={searchSVG} alt="Search Icon" />
             </div>
         </div>
-    )
+    );
 }
