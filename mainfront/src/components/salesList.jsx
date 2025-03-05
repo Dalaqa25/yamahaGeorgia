502d58deba45a8c08c0073f0d551625173ce694c
dalaqa25 <g.dalaqishvili01@gmail.com>
2025-03-05 23:36:06 +0400

    ds

--- mainfront/src/components/salesList.jsx
+++ mainfront/src/components/salesList.jsx
@@ -1,34 +1,26 @@
 import '../../src/assets/sale.css'
 import r6img from '../../src/assets/images/r6.png'
-
+import { useState } from 'react'
 
 export default function SalesList() {
+
+    const [ isHoverd, setIshoverd] = useState(false)
+
     return (
         <div className='main'>
             <div className='saleList-container'>
-                <div className='box'>
+                <div className='box'
+                        onMouseEnter={() => setIshoverd(true)}
+                        onMouseLeave={() => setIshoverd(false)}>
                     <img src={r6img} alt="r6img" />
-                </div>
-                <div className='box'>
-                    <img src={r6img} alt="r6img" />
-                </div>
-                <div className='box'>
-                    <img src={r6img} alt="r6img" />
-                </div>
-                <div className='box'>
-                    <img src={r6img} alt="r6img" />
-                </div>
-                <div className='box'>
-                    <img src={r6img} alt="r6img" />
-                </div>
-                <div className='box'>
-                    <img src={r6img} alt="r6img" />
-                </div>
-                <div className='box'>
-                    <img src={r6img} alt="r6img" />
-                </div>
-                <div className='box'>
-                    <img src={r6img} alt="r6img" />
+                    <div style={{display:'flex', gap:'10px'}}>
+                        <p style={{color:'#000'}}>YZF R6</p>
+                        <hr />
+                        <p style={{color:'#000'}}>In stock</p>
+                    </div>
+                    {isHoverd && (
+                        <button className='readMoreBtn'>Learn more</button>
+                    )}
                 </div>
             </div> 
         </div>
