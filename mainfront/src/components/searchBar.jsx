59b582b10a6898afad285df47ca320f774567053
dalaqa25 <g.dalaqishvili01@gmail.com>
2025-03-12 10:32:51 +0400

    search result bug

--- mainfront/src/components/searchBar.jsx
+++ mainfront/src/components/searchBar.jsx
@@ -8,7 +8,7 @@ export default function SearchBar() {
     const [results, setResults] = useState([]);
     const [placeholder, setPlaceholder] = useState("Search for product");
     const [isOverlayVisible, setOverlayVisible] = useState(false);
-    const [searchResults, setSearchResults] = useState(false);
+    const [searchResultVisible, setSearchResultVisible] = useState(false);
 
     const fetchData = (value) => {
         fetch("https://jsonplaceholder.typicode.com/users")
@@ -32,43 +32,43 @@ export default function SearchBar() {
         fetchData(value);
     };
 
-    function toggleSearchResults() {
-        setSearchResults(prevResults => !prevResults);
-    }
-
     return (
-        <div className="search-bar-container">
-            <div className={`overlay ${isOverlayVisible ? "show" : ""}`} onClick={() => setOverlayVisible(false)}></div>
-            <div className="search-bar">
-                <div className='search-bar-wrapper' onClick={toggleSearchResults}>
-                    <input
-                        type="text"
-                        style={{ textAlign: 'center' }}
-                        placeholder={placeholder}
-                        value={input}
-                        onChange={(e) => handleChange(e.target.value)}
-                        onFocus={() => {
-                            setPlaceholder("");
-                            setOverlayVisible(true);
-                        }}
-                        onBlur={(e) => {
-                            if (e.target.value === "") {
-                                setPlaceholder("Search for product");
-                            }
-                            setOverlayVisible(false);
-                        }}
-                    />
-                    <img src={searchSVG} alt="Search Icon" />
+        <>
+        <div className={`overlay ${isOverlayVisible ? "show" : ""}`} onClick={() => setOverlayVisible(false)}></div>
+
+            <div className="search-bar-container"> 
+                <div className="search-bar">
+                    <div className='search-bar-wrapper' onClick={ () => setSearchResultVisible(!searchResultVisible)}>
+                        <input
+                            type="text"
+                            style={{ textAlign: 'center' }}
+                            placeholder={placeholder}
+                            value={input}
+                            onChange={(e) => handleChange(e.target.value)}
+                            onFocus={() => {
+                                setPlaceholder("");
+                                setOverlayVisible(true);
+                            }}
+                            onBlur={(e) => {
+                                if (e.target.value === "") {
+                                    setPlaceholder("Search for product");
+                                }
+                                setOverlayVisible(false);
+                            }}
+                        />
+                        <img src={searchSVG} alt="Search Icon" />
+                    </div>
                 </div>
-                <div className="search-result-list">
-                    {results.map((result, index) => (
-                        <div key={index} className="search-result-item">
-                            {result.name}
-                        </div>
-                    ))} 
-                   
+                <div className={`search-result-list ${searchResultVisible ? "showResult": "NotShowResult"}`}>
+                        {results.map((result, index) => (
+                            <div key={index} className="search-result-item">
+                                {result.name}
+                            </div>
+                        ))} 
                 </div>
             </div>
-        </div>
+            
+        </>
+ 
     );
 }
