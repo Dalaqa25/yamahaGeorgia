a83ee412058db15f22805a4e26644360188c561f
dalaqa25 <g.dalaqishvili01@gmail.com>
2025-03-22 11:40:49 +0400

    database has been succsesfully set uped

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
+import { sql } from "./config/db.js";
 
 dotenv.config();
 
@@ -18,6 +19,31 @@ app.use(morgan("dev")); // log requests
 //get request
 app.use("/api/products", productRoutes)
 
-app.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
-})
+async function initDB() {
+    try {
+        await sql `
+            CREATE TABLE IF NOT EXISTS motorcycles (
+                id SERIAL PRIMARY KEY,
+                brand VARCHAR(255) NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                image VARCHAR(255) NOT NULL,
+                price DECIMAL(10, 2) NOT NULL,
+                engine VARCHAR(255) NOT NULL,
+                inStock BOOLEAN NOT NULL,
+                transmission VARCHAR(255) NOT NULL,
+                year INT NOT NULL,
+                description TEXT NOT NULL,
+                createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `
+        console.log("Database initialized!")
+    } catch(error) {
+        console.log("error while initDB", error)
+    }
+}
+
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server running on port " + PORT);
+    })    
+})  
